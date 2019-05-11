a4cd2f48c984e39100ab6a5cb97cfcb1f6749724
apohrebniak <andrii.pohrebniak@gmail.com>
2019-05-11 17:05:15 +0300

    start

---
+++ hover-kv-ui/src/main/java/com/github/apohrebniak/hoverui/HoverKvUiApplication.java
@@ -0,0 +1,13 @@
+package com.github.apohrebniak.hoverui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoverKvUiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HoverKvUiApplication.class, args);
+  }
+
+}
