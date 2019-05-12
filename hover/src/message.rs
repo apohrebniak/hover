c5c3581b5423c723a46463e6a921240cbe498d67
apohrebniak <andrii.pohrebniak@gmail.com>
2019-05-12 17:35:57 +0300

    fix

--- hover/src/message.rs
+++ hover/src/message.rs
@@ -74,7 +74,7 @@ impl MessageDispatcher {
     }
 
     fn handle_response(&self, msg: Arc<Message>) {
-        match self.resp_callbacks.write().unwrap().remove(&msg.cor_id) {
+        match self.resp_callbacks.read().unwrap().get(&msg.cor_id) {
             Some(sender) => {
                 sender.send(msg);
             }
@@ -302,7 +302,7 @@ impl MessagingService {
 
         //add message listener for given correlation id
         self.message_dispatcher
-            .write()
+            .read()
             .unwrap()
             .add_resp_callback(correlation_id, s);
 
@@ -310,7 +310,7 @@ impl MessagingService {
             Err(err) => {
                 eprintln!("[MessageSercive]: Error while sending a message!");
                 self.message_dispatcher
-                    .write()
+                    .read()
                     .unwrap()
                     .remove_resp_callback(correlation_id);
                 return Err(err);
@@ -322,7 +322,7 @@ impl MessagingService {
         return match r.recv_timeout(timeout) {
             Ok(response) => {
                 self.message_dispatcher
-                    .write()
+                    .read()
                     .unwrap()
                     .remove_resp_callback(correlation_id);
                 Ok(response)
@@ -330,7 +330,7 @@ impl MessagingService {
             Err(err) => {
                 eprintln!("[MessageService]: Error while waiting for the response!");
                 self.message_dispatcher
-                    .write()
+                    .read()
                     .unwrap()
                     .remove_resp_callback(correlation_id);
                 Err(Box::new(err))
